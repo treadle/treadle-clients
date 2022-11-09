68450851ee2ae69fecb48c7b03330f9e73bda6ad
pantemon <pantemonnn@gmail.com>
2022-11-09 00:06:18 +0000

    Add contract's roadmap & versions

---
+++ packages/treadle-mockup-server/README.md
@@ -0,0 +1,15 @@
+# TRDLB Contract
+This smart contracts is used to issue TRDL Bikes. It is based on the NEP-171 and has its specific additional methods e.g. `burn_nft(...)` and `nft_metadata_edit(...)`.
+
+## Versions
+
+### 0.1.0 ✅
+Implemented NEP-171 standart.
+
+### 0.2.0 🚧
+Implemented the following methods:
+- [] `nft_metadata_edit(...)`
+- [] `nft_burn(...)`
+
+### X.Y.Z ???
+TRDLB Contract allows to craft TRDL Bikes NFT from TRDL Components NFT using $SCRW fungible token in a fully decentralised manner.
--- packages/treadle-mockup-server/src/MockupServer.ts
+++ packages/treadle-mockup-server/src/MockupServer.ts
@@ -4,6 +4,68 @@ import BN from "bn.js";
 import { CONTRACT_NAME } from "./config";
 import { ConnectConfig } from "near-api-js";
 
+const ONE_TENTH_OF_A_NEAR = () => {
+  return new BN("100000000000000000000000");
+}
+
+// interface TRDLBBikeStats {
+//   durability: number;
+//   wear: number;
+//   efficiency: number;
+//   comfort: number;
+// }
+
+export interface TRDLBTokenMetadata {
+  title: string;
+  description: string;
+  media: string;
+  extra: string;
+}
+
+export class TRDLBContract {
+  account: nearAPI.Account;
+  contractId: string;
+  
+  constructor(account: nearAPI.Account, contractId: string) {
+    this.account = account;
+    this.contractId = contractId;
+  }
+
+  async nft_mint(receiverId: string, metadata: TRDLBTokenMetadata) {
+    const account = this.account;
+    const contractId = this.contractId;
+
+    const tokenId = `${Date.now() + Math.floor(Math.random() * 1e8)}`;
+
+    return await account.functionCall({
+      contractId: contractId, 
+      methodName: "nft_mint",
+      args: {
+        "token_id": tokenId,
+        "receiver_id": receiverId, 
+        "token_metadata": metadata,
+      },
+      attachedDeposit: ONE_TENTH_OF_A_NEAR(), // 0.1 NEAR
+    });
+  }
+
+  async nft_burn(tokenId: string) {
+    throw new Error("NOT IMPLEMENTED");
+  }
+
+  async nft_metadata_edit(tokenId: string, metadata: TRDLBTokenMetadata) {
+    throw new Error("NOT IMPLEMENTED");
+  }
+
+  // To-Do:
+  // from_index: Option<U128>
+  // limit: Option<u64>
+  async nft_tokens_for_owner(accountId: string) {
+    throw new Error("NOT IMPLEMENTED");
+  }
+}
+
+
 export class MockupServer {
   near: nearAPI.Near;
   masterAccount: nearAPI.Account;
@@ -28,13 +90,13 @@ export class MockupServer {
   async fetchBikesForOwner(accountId: string) {
     const masterAccount = this.masterAccount;
 
-    return await masterAccount.viewFunction(
-      CONTRACT_NAME, 
-      "nft_tokens_for_owner", 
-      {
-        "account_id": accountId,
-      }
-    );
+    return await masterAccount.viewFunctionV2({ 
+      contractId: CONTRACT_NAME, 
+      methodName: "nft_tokens_for_owner", 
+      args: { 
+        "account_id": accountId 
+      } 
+    });
   }
 
   async mintBike(receiverId: string) {
@@ -59,12 +121,10 @@ export class MockupServer {
 }
 
 // Initialize contract & set global variables
-export async function setupMockupServer() {
-  const MASTER_PRIVATE_KEY = 'jNpUvccrCML6r4SRYkrVXfyiRDf61pyXh4UP6jwaEjYhVJbM6kWXZn86mt3w6hB4uhr3Xrhw2wmseUVA6Jetd9E';
-
+export async function setupMockupServer(privateKey: string) {
   const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
   
-  const keyPair = nearAPI.KeyPair.fromString(MASTER_PRIVATE_KEY);
+  const keyPair = nearAPI.KeyPair.fromString(privateKey);
   await keyStore.setKey('testnet', 'pantemon.testnet', keyPair);
 
   const connectionConfig: ConnectConfig = {
@@ -77,25 +137,13 @@ export async function setupMockupServer() {
   
   // Initialize connection to the NEAR testnet
   const near = await nearAPI.connect(connectionConfig);
-
   const account = await near.account("pantemon.testnet");
 
-  // account.viewFunction()
+  const server = new MockupServer(near, account);
 
-  // // Initializing our contract APIs by contract name and configuration
-  
-  // const contract = new nearAPI.Contract(
-  //   account,
-  //   config.contractName,
-  //   {
-  //     // View methods are read only. They don't modify the state, but usually return some value.
-  //     viewMethods: ["nft_tokens_for_owner"],
-  //     // Change methods can modify the state. But you don't receive the returned value when called.
-  //     changeMethods: ["nft_mint"],
-  //   }
-  // );
-
-  // contract.account.viewFunction()
-
-  return new MockupServer(near, account);
+  return {
+    server: server,
+    near: near,
+    account: account,
+  };
 }
