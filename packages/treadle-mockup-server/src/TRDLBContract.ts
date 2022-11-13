de336b4e171f10ea9b37a441526ce6ab499b0478
pantemon <pantemonnn@gmail.com>
2022-11-13 18:05:40 +0000

    Add unit tests for utils

--- packages/treadle-mockup-server/src/TRDLBContract.ts
+++ packages/treadle-mockup-server/src/TRDLBContract.ts
@@ -1,18 +1,35 @@
+import BN from 'bn.js';
 import * as nearAPI from 'near-api-js';
 import * as utils from './utils';
 
-// // interface TRDLBBikeStats {
-// //   durability: number;
-// //   wear: number;
-// //   efficiency: number;
-// //   comfort: number;
-// // }
+export interface TRDLBTokenMetadataExtra {
+  durability: number; // measured in (units),
+  ware: number; // measured in (units * energy ^ -1), accuracy is (up to 2 decimal places)
+  efficiency: number; // measured in (tokens * energy ^ -1), accuracy is (up to 2 decimal places)
+  comfort: number; // measured in (energy * kilometre ^ -1), accuracy is (up to 2 decimal places)
+}
 
-export interface TRDLBTokenMetadata {
-  title: string;
-  description: string;
-  media: string;
-  extra: string;
+export interface TRDLBJsonTokenMetadata {
+  title: string | null;
+  description: string | null;
+  media: string | null;
+  media_hash?: string | null;
+  copies?: number | null;
+  issued_at?: number | null;
+  expires_at?: number | null;
+  starts_at?: number | null;
+  updated_at?: number | null;
+  extra: string | null;
+  reference?: string | null;
+  reference_hash?: string | null;
+}
+
+export interface TRDLBJsonToken {
+  token_id: string;
+  owner_id: string;
+  metadata: TRDLBJsonTokenMetadata,
+  approved_account_ids: object,
+  royalty: object,
 }
 
 export class TRDLBContract {
@@ -24,36 +41,84 @@ export class TRDLBContract {
     this.contractId = contractId;
   }
 
-  async nft_mint(receiverId: string, metadata: TRDLBTokenMetadata) {
+  async nft_mint(options: TRDLBNftMintOptions) {
     const account = this.account;
     const contractId = this.contractId;
 
-    const tokenId = `${Date.now() + Math.floor(Math.random() * 1e8)}`;
+    const token_id = (options.token_id) ? options.token_id : `${Date.now() + Math.floor(Math.random() * 1e8)}`;
 
     return await account.functionCall({
       contractId: contractId, 
       methodName: "nft_mint",
       args: {
-        "token_id": tokenId,
-        "metadata": metadata,
-        "receiver_id": receiverId, 
+        "token_id": token_id,
+        "metadata": options.metadata,
+        "receiver_id": options.receiver_id, 
       },
       attachedDeposit: utils.ONE_TENTH_OF_A_NEAR(), // 0.1 NEAR
     });
   }
 
-  async nft_burn(tokenId: string) {
-    throw new Error("NOT IMPLEMENTED");
-  }
+  async nft_token_metadata_edit(options: TRDLBNftTokenMetadataEditOptions) {
+    const account = this.account;
+    const contractId = this.contractId;
 
-  async nft_metadata_edit(tokenId: string, metadata: TRDLBTokenMetadata) {
-    throw new Error("NOT IMPLEMENTED");
+    return await account.functionCall({
+      contractId: contractId, 
+      methodName: "nft_token_metadata_edit",
+      args: {
+        "token_id": options.token_id,
+        "metadata": options.metadata,
+      },
+      attachedDeposit: utils.ONE_TENTH_OF_A_NEAR(), // 0.1 NEAR
+    });
   }
 
   // To-Do:
   // from_index: Option<U128>
   // limit: Option<u64>
-  async nft_tokens_for_owner(accountId: string) {
-    throw new Error("NOT IMPLEMENTED");
+  async nft_tokens_for_owner(options: TRDLBNftTokensForOwnerOptions) {
+    const account = this.account;
+    const contractId = this.contractId;
+
+    return await account.viewFunctionV2({ 
+      contractId: contractId, 
+      methodName: "nft_tokens_for_owner", 
+      args: options, 
+    });
   }
+
+  async nft_token(options: TRDLBNftTokenOptions) {
+    const account = this.account;
+    const contractId = this.contractId;
+
+    return await account.viewFunctionV2({ 
+      contractId: contractId, 
+      methodName: "nft_token", 
+      args: {
+        "token_id": options.token_id,
+      }, 
+    });
+  }
+}
+
+export interface TRDLBNftMintOptions {
+  token_id?: string;
+  metadata: TRDLBJsonTokenMetadata,
+  receiver_id: string,
+}
+
+export interface TRDLBNftTokensForOwnerOptions {
+  account_id: string,
+  from_index?: BN,
+  limit?: number,
+}
+
+export interface TRDLBNftTokenOptions {
+  token_id: string,
+}
+
+export interface TRDLBNftTokenMetadataEditOptions {
+  token_id: string,
+  metadata: TRDLBJsonTokenMetadata,
 }
