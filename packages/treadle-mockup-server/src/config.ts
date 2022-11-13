f67bfe66ceb12ee001e987c60ee284f4ba20c45a
pantemon <pantemonnn@gmail.com>
2022-11-13 12:33:54 +0000

    Remove unused config

--- packages/treadle-mockup-server/src/config.ts
+++ packages/treadle-mockup-server/src/config.ts
@@ -1,30 +1,34 @@
-export const CONTRACT_NAME = "dev-1667937803383-86710269890914";
 
-export function generateConfig(environment: string) {
-  switch (environment) {
-    case "production":
-    case "mainnet":
-      return {
-        networkId: "mainnet",
-        nodeUrl: "https://rpc.mainnet.near.org",
-        contractName: CONTRACT_NAME,
-        walletUrl: "https://wallet.near.org",
-        helperUrl: "https://helper.mainnet.near.org",
-        explorerUrl: "https://explorer.mainnet.near.org",
-      };
-    case "development":
-    case "testnet":
-      return {
-        networkId: "testnet",
-        nodeUrl: "https://rpc.testnet.near.org",
-        contractName: CONTRACT_NAME,
-        walletUrl: "https://wallet.testnet.near.org",
-        helperUrl: "https://helper.testnet.near.org",
-        explorerUrl: "https://explorer.testnet.near.org",
-      }; 
-    default:
-      throw Error(
-        `Unconfigured environment '${environment}'. Can be configured in src/config.js.`
-      );
-  }
-}
+
+// export const CONTRACT_NAME = "dev-1667937803383-86710269890914";
+
+// export function generateConfig(environment: string) {
+//   switch (environment) {
+//     case "production":
+//     case "mainnet":
+//       return {
+//         networkId: "mainnet",
+//         nodeUrl: "https://rpc.mainnet.near.org",
+//         contractName: CONTRACT_NAME,
+//         walletUrl: "https://wallet.near.org",
+//         helperUrl: "https://helper.mainnet.near.org",
+//         explorerUrl: "https://explorer.mainnet.near.org",
+//       };
+//     case "development":
+//     case "testnet":
+//       return {
+//         networkId: "testnet",
+//         nodeUrl: "https://rpc.testnet.near.org",
+//         contractName: CONTRACT_NAME,
+//         walletUrl: "https://wallet.testnet.near.org",
+//         helperUrl: "https://helper.testnet.near.org",
+//         explorerUrl: "https://explorer.testnet.near.org",
+//       }; 
+//     default:
+//       throw Error(
+//         `Unconfigured environment '${environment}'. Can be configured in src/config.js.`
+//       );
+//   }
+// }
+
+
