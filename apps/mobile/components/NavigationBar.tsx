c748c365b802e53697db22712d24a1279b18a94a
Vladyslav Chuchupak <v.chuchupak@gmail.com>
2022-11-13 15:01:44 +0100

    fix: load error

--- apps/mobile/components/NavigationBar.tsx
+++ apps/mobile/components/NavigationBar.tsx
@@ -1,12 +1,12 @@
 import type { BottomTabHeaderProps } from '@react-navigation/bottom-tabs';
 import type { FC } from 'react';
 import { Text, View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import { Appbar, MD3DarkTheme } from 'react-native-paper';
 
 const NavigationBar: FC<BottomTabHeaderProps> = ({ route }) => {
   return (
-    <Appbar.Header>
-      <Appbar.Content title={route.name} />
+    <Appbar.Header theme={MD3DarkTheme}>
+      <Appbar.Content theme={MD3DarkTheme} title={route.name} />
       <View className='p-2 bg-red-700'>
         <Text>Energy</Text>
       </View>
