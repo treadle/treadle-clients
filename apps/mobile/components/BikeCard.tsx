0f4bcafa654822562a52933aead4714395cf1a00
Vladymyr Sheremeta <sh.vladymyr@gmail.com>
2022-11-16 18:28:00 +0100

    small percentage line fix

--- apps/mobile/components/BikeCard.tsx
+++ apps/mobile/components/BikeCard.tsx
@@ -13,9 +13,9 @@ const BikeCard: FC<CardProps> = ({ bikeMetadata }) => {
 
   const stats = JSON.parse(bikeMetadata.extra as string)
   const durabilityPercentage = (stats.durability / 200).toString() + '%';
-  const warePercentage = (stats.ware / 10).toString() + '%';
+  const warePercentage = (110 - stats.ware / 10).toString() + '%';
   const efficiencyPercentage = (stats.efficiency / 10).toString() + '%';
-  const comfortPercentage = (stats.comfort / 10).toString() + '%';
+  const comfortPercentage = (110 - stats.comfort / 10).toString() + '%';
 
   return (
     <View className="bg-md3-surface flex-1 items-center rounded-[12px] overflow-hidden border border-md3-outline-variant">
